49b65ccea8772c0b069770e84fc006dcac1a54bf
nss.nosystemissafe@gmail.com <agafonov.egorushka@gmail.com>
2024-03-30 04:35:48 +0400

    Add FLASK_ENDPOINT into .env

--- start.sh
+++ start.sh
@@ -6,11 +6,11 @@ export $(grep -v '^#' .env | xargs)
 # Проверяем значение START_MODE
 if [ "$START_MODE" = "dev" ]; then
     # Запускаем контейнеры в режиме разработки, включая db
-    MONGODB_URI=$MONGODB_URI docker-compose up --build
+    MONGODB_URI=$MONGODB_URI FLASK_ENDPOINT=$FLASK_ENDPOINT docker-compose up --build
 elif [ "$START_MODE" = "prod" ]; then
     # Запускаем контейнеры в режиме продакшена, не включая db
     # Используем переменную окружения для MONGODB_URI
-    MONGODB_URI=$MONGODB_URI docker-compose up --build --scale db=0
+    MONGODB_URI=$MONGODB_URI FLASK_ENDPOINT=$FLASK_ENDPOINT docker-compose up --build --scale db=0
 else
     echo "Неизвестный режим START_MODE: $START_MODE"
     exit 1
