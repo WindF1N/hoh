42851e9a1499d75d1134a4fc3633e0c3c2ccdb4e
nss.nosystemissafe@gmail.com <agafonov.egorushka@gmail.com>
2024-04-09 04:27:18 +0400

    change .env and add .env.example

--- backend/mail.py
+++ backend/mail.py
@@ -1,18 +1,22 @@
+import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_email(to, subject, message):
     msg = MIMEMultipart()
-    msg['From'] = 'agafonov.egorushka@gmail.com'
+    msg['From'] = os.getenv('MAIL_LOGIN', None)
     msg['To'] = to
     msg['Subject'] = subject
     msg.attach(MIMEText(message))
 
-    mailserver = smtplib.SMTP('smtp.gmail.com', 587)
+    mailserver = smtplib.SMTP(os.getenv('MAIL_SERVER', None), os.getenv('MAIL_SERVER_PORT', None))
     mailserver.ehlo()
     mailserver.starttls()
     mailserver.ehlo()
-    mailserver.login('agafonov.egorushka@gmail.com', 'tnqj nlsw dmtg kejl')
-    mailserver.sendmail('agafonov.egorushka@gmail.com', to, msg.as_string())
+    mailserver.login(os.getenv('MAIL_LOGIN', None), os.getenv('MAIL_PASSWORD', None))
+    mailserver.sendmail(os.getenv('MAIL_LOGIN', None), to, msg.as_string())
     mailserver.quit()
